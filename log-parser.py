ddfec4f778854d79802bfa58cba04dfd6d35f038
Ian Baker <ian@icbaker.co.uk>
2014-05-18 18:00:34 +0100

    Add sample frequency extraction

--- log-parser.py
+++ log-parser.py
@@ -3,6 +3,21 @@ from xml.etree import ElementTree
 import argparse
 import sys
 
+XML_SCHEMA = '{http://schemas.datacontract.org/2004/07/Suunto.Diving.Dal}'
+
+def main():
+  args = parse_args()
+  root = ElementTree.parse(args.logFile).getroot()
+  depths = extract_depths(root.find(XML_SCHEMA + 'DiveSamples'))
+  sampleFreq = extract_sample_freq(root)
+  print depths
+  print sampleFreq
+
+def parse_args():
+  parser = argparse.ArgumentParser(description='Extract dive log data into a .srt file')
+  parser.add_argument('--xml_log_file', '-f', required=True, dest='logFile')
+  return parser.parse_args()
+
 def extract_depths(diveSamples):
   assert diveSamples.tag.endswith('DiveSamples')
   depths = []
@@ -10,20 +25,10 @@ def extract_depths(diveSamples):
     for child in diveSample:
       if child.tag.endswith('Depth'):
         depths.append(float(child.text))
-
   return depths
-
-def parse_args():
-  parser = argparse.ArgumentParser(description='Extract dive log data into a .srt file')
-  parser.add_argument('--xml_log_file', '-f', required=True, dest='logFile')
-  return parser.parse_args()
-
-def main():
-  args = parse_args()
   
-  root = ElementTree.parse(args.logFile).getroot()
-  depths = extract_depths(root[13])
-  print depths
+def extract_sample_freq(root):
+  return int(root.find(XML_SCHEMA + 'SampleInterval').text)
 
 if __name__ == "__main__":
     main()
